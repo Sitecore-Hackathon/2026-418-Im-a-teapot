a09fdc5cefaac3d0828cba9bf09d753e32437aaf
Jesper Balle <jba@asmblii.com>
2026-03-07 21:04:56 +0100

    Proper url for registered webhook

--- marketplaceapp/components/initialization/webhook-actions.ts
+++ marketplaceapp/components/initialization/webhook-actions.ts
@@ -2,25 +2,31 @@ import {
     type ApplicationContext,
     ClientSDK,
 } from "@sitecore-marketplace-sdk/client";
-import { createWebhookQuery, updateWebhookQuery, WebhookItem } from "./webhook-queries";
+import { makeCreateWebhookQuery, makeUpdateWebhookQuery, WebhookItem } from "./webhook-queries";
 
-export async function createWebhookItem(appContext: ApplicationContext, client: ClientSDK) : Promise<[WebhookItem?, string?]> {
+export async function createWebhookItem(appContext: ApplicationContext, client: ClientSDK): Promise<[WebhookItem?, string?]> {
     const xmCloudResourceAccess = appContext?.resourceAccess?.[0];
     const sitecoreContextId = xmCloudResourceAccess?.context.preview;
-    if(!sitecoreContextId) {
+    const instanceId = appContext?.installationId;
+
+    if (!sitecoreContextId || !instanceId) {
         return [undefined, "Missing context id or item"];
     }
 
+    const request = makeCreateWebhookQuery(sitecoreContextId, instanceId);
+
     const response: any = await client.mutate("xmc.authoring.graphql", {
-        params: {
-            query: { sitecoreContextId },
-            body: {
-                query: createWebhookQuery,
-            }
-        }
+        params: request,
     });
     const error = response?.error ?? response?.data?.errors;
     const resultItem: WebhookItem = response?.data?.data?.data?.item;
+
+    console.debug('Creating webhook for Marketplace', {
+        resultItem,
+        error,
+        response,
+        request,
+    });
     return [
         resultItem, error
     ];
@@ -29,25 +35,25 @@ export async function createWebhookItem(appContext: ApplicationContext, client: 
 export async function updateWebhookItem(appContext: ApplicationContext, client: ClientSDK, existingItem: WebhookItem): Promise<[WebhookItem?, string?]> {
     const xmCloudResourceAccess = appContext?.resourceAccess?.[0];
     const sitecoreContextId = xmCloudResourceAccess?.context.preview;
-    if(!sitecoreContextId || !existingItem?.itemId) {
+    const instanceId = appContext?.installationId;
+
+    if (!sitecoreContextId || !instanceId || !existingItem?.itemId) {
         return [undefined, "Missing context id or item"];
     }
 
-    console.log(updateWebhookQuery);
-
+    const request = makeUpdateWebhookQuery(sitecoreContextId, instanceId, existingItem.itemId)
     const response: any = await client.mutate("xmc.authoring.graphql", {
-        params: {
-            query: { sitecoreContextId },
-            body: {
-                query: updateWebhookQuery,
-                variables: {
-                    itemId: existingItem.itemId
-                }
-            }
-        }
+        params: request,
     });
     const error = response?.error ?? response?.data?.errors;
     const resultItem: WebhookItem = response?.data?.data?.data?.item;
+
+    console.debug('Updating webhook for Marketplace', {
+        resultItem,
+        error,
+        response,
+        request,
+    });
     return [
         resultItem, error
     ];
