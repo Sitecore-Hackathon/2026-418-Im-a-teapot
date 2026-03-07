50b94630f468e40c6fb61fc83800da3c74e0108f
Jesper Balle <jba@asmblii.com>
2026-03-07 16:43:22 +0100

    Initialization

--- marketplaceapp/components/initialization/InitializationPage.tsx
+++ marketplaceapp/components/initialization/InitializationPage.tsx
@@ -1,17 +1,25 @@
-import { useAppContext, useMarketplaceClient } from "@/components/providers/marketplace";
-import {
-  Collapsible,
-} from "@/components/ui/collapsible";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Icon } from "@/lib/icon";
+import { mdiAlertOutline, mdiCheckCircleOutline } from "@mdi/js";
+import { useAppContext, useMarketplaceClient } from "@/components/providers/marketplace";
 import { useWebhookState } from "./webhook-hook";
-import { Button } from "../ui/button";
 import { createWebhookItem, updateWebhookItem } from "./webhook-actions";
 import { type WebhookItem } from "./webhook-queries";
 
 export const InitializationPage = () => {
   const appContext = useAppContext();
   const client = useMarketplaceClient();
-  const [isExpanded, setIsExpanded] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
   const [webhookItem, setWebhookItem] = useWebhookState(appContext, client);
 
@@ -24,7 +32,7 @@ export const InitializationPage = () => {
     if (error) {
       console.error(error);
     }
-  }
+  };
 
   const handleCreate = async () => {
     const [resultItem, error] = await createWebhookItem(appContext, client);
@@ -36,15 +44,28 @@ export const InitializationPage = () => {
     actionResult(resultItem, error);
   };
 
-  return (
-    <Collapsible
-      open={isExpanded}
-      onOpenChange={setIsExpanded}
-      className="border-[1px] rounded-lg"
-    >
+  const stateOk = webhookItem?.enabled;
+  const stateText = stateOk ? 'Configured' : 'Not configured';
+
+  return (<Dialog>
+    <DialogTrigger asChild>
+      <Button colorScheme={stateOk ? "success" : "danger"}>
+        <Icon path={stateOk ? mdiCheckCircleOutline : mdiAlertOutline} className="mr-2" />
+        {stateText}
+      </Button>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Webhook installation status</DialogTitle>
+        <DialogDescription>Install or update webhook integration</DialogDescription>
+      </DialogHeader>
+
       <dl>
         <dt>Item id:</dt>
         <dd>{webhookItem?.itemId}</dd>
+        <dt>Enabled:</dt>
+        <dd>{webhookItem?.enabled?.value ? 'true' : 'false'}</dd>
+
       </dl>
 
       {isUpdating && <span>Updating...</span>}
@@ -52,6 +73,14 @@ export const InitializationPage = () => {
       {webhookItem && <Button onClick={() => handleUpdate()} title="Update webhook">Update</Button>}
       {!webhookItem && <Button onClick={() => handleCreate()} title="Create webhook">Create</Button>}
 
-    </Collapsible>
+      <DialogFooter>
+        <DialogClose asChild>
+          <Button variant="ghost" colorScheme="neutral">
+            Cancel
+          </Button>
+        </DialogClose>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
   );
 };
