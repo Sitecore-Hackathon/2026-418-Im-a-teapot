05d01d583ce15abdf08d821299fc58fd63e513bd
Per Bering <pbering@fastmail.com>
2026-03-07 22:58:33 +0100

    query by ids

--- coordinatorapi/src/CoordinatorApi/Query/QueryService.cs
+++ coordinatorapi/src/CoordinatorApi/Query/QueryService.cs
@@ -1,5 +1,6 @@
 using CoordinatorApi.Shared;
 using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 
 namespace CoordinatorApi.Query;
 
@@ -9,16 +10,31 @@ public class QueryService(ElasticsearchClient client)
 
     public async Task<IEnumerable<IndexChangeModel>> GetItems(string sitecoreInstanceId, string ids, HttpContext http, CancellationToken cancellationToken)
     {
+        var itemIds = new List<FieldValue>();
+
+        foreach (var id in ids.Split(';'))
+        {
+            itemIds.Add(FieldValue.String(id));
+        }
+
         var response = await client.SearchAsync<IndexChangeModel>(s => s
             .Indices(_indexName)
             .From(0)
             .Size(100)
             .Query(q => q
-                .Term(t => t
-                    .Field(x => x.SitecoreInstance)
-                    .Value(sitecoreInstanceId)
+                .Bool(b => b
+                    .Filter(
+                        f => f.MatchPhrase(mp => mp
+                            .Field("sitecoreInstance.keyword")
+                            .Query(sitecoreInstanceId)
+                        ), f => f.Terms(t => t
+                            .Field("itemId.keyword")
+                            .Terms(new TermsQueryField(itemIds))
+                        )
+                    )
                 )
             )
+            .Sort(ss => ss.Field(f => f.Timestamp, SortOrder.Desc))
         );
 
         if (!response.IsValidResponse)
--- coordinatorapi/src/CoordinatorApi/Shared/IndexChangeModel.cs
+++ coordinatorapi/src/CoordinatorApi/Shared/IndexChangeModel.cs
@@ -4,25 +4,25 @@ namespace CoordinatorApi.Shared;
 
 public class IndexChangeModel
 {
-    public DateTime Timestamp { get; init; }
+    public DateTime Timestamp { get; set; }
 
-    public required string EventName { get; init; }
+    public required string EventName { get; set; }
 
-    public Guid ItemId { get; init; }
+    public Guid ItemId { get; set; }
 
-    public Guid ParentId { get; init; }
+    public Guid ParentId { get; set; }
 
     public int Version { get; set; }
 
     public string? Language { get; set; }
 
-    public Guid[]? FieldIds { get; init; }
+    public Guid[]? FieldIds { get; set; }
 
-    public required string Raw { get; init; }
+    public required string Raw { get; set; }
 
-    public string? User { get; init; }
+    public string? User { get; set; }
 
-    public required string SitecoreInstance { get; init; }
+    public required string SitecoreInstance { get; set; }
 
-    public string? ChangedFields { get; init; }
+    public string? ChangedFields { get; set; }
 }
